3d89b5f42022d88ed493f0bea73d2cda92f4a14b
SUWON <entryswlee@gmail.com>
2020-10-26 23:58:13 +0900

    2주차: 타이틀 스테이트 수정

--- TermProject/title_state.py
+++ TermProject/title_state.py
@@ -20,7 +20,7 @@ class TitleState:
         core.renderer.Add(title_background)
 
         self.title_text = ui.UI('./res/title_text.png')
-        self.title_text.spr.x = -200.0
+        self.title_text.spr.x = -self.title_text.spr.image.w
         self.title_text.spr.y = core.const.SCREEN_HEIGHT - 10.0
         self.title_text.spr.origin_x = 0.0
         self.title_text.spr.origin_y = 1.0
@@ -50,19 +50,19 @@ class TitleState:
 
         if self.pressed_start_button is False and self.pressed_exit_button is False:
             self.title_text.move_to(10.0, self.title_text.spr.y, UI_MOVE_SPEED)
-            self.score_box.move_to(self.score_box.spr.x, 290.0, UI_MOVE_SPEED)
-            self.start_button.move_to(self.start_button.spr.x, 200.0, UI_MOVE_SPEED)
-            self.exit_button.move_to(self.exit_button.spr.x, 200.0, UI_MOVE_SPEED * 0.75)
+            self.score_box.move_to(self.score_box.spr.x, 280.0, UI_MOVE_SPEED)
+            self.start_button.move_to(self.start_button.spr.x, 185.0, UI_MOVE_SPEED)
+            self.exit_button.move_to(self.exit_button.spr.x, 185.0, UI_MOVE_SPEED * 0.75)
 
             if self.start_button.is_pressed():
                 self.pressed_start_button = True
             elif self.exit_button.is_pressed():
                 self.pressed_exit_button = True
         else:
-            self.title_text.move_to(-200.0, self.title_text.spr.y, UI_MOVE_SPEED)
-            self.score_box.move_to(self.score_box.spr.x, core.const.SCREEN_HEIGHT, UI_MOVE_SPEED)
-            self.start_button.move_to(self.start_button.spr.x, -self.start_button.spr.image.h, UI_MOVE_SPEED)
-            self.exit_button.move_to(self.exit_button.spr.x, -self.exit_button.spr.image.h, UI_MOVE_SPEED)
+            self.title_text.move_to(-200.0, self.title_text.spr.y, UI_MOVE_SPEED * 1.5)
+            self.score_box.move_to(self.score_box.spr.x, core.const.SCREEN_HEIGHT, UI_MOVE_SPEED * 1.5)
+            self.start_button.move_to(self.start_button.spr.x, -self.start_button.spr.image.h, UI_MOVE_SPEED * 1.5)
+            self.exit_button.move_to(self.exit_button.spr.x, -self.exit_button.spr.image.h, UI_MOVE_SPEED * 1.5)
 
             self.next_state_delay -= core.delta_time
             if self.next_state_delay <= 0.0:
