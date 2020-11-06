92ec59e71f60a70bca14bb4ccda3bf7699fc224b
SUWON <entryswlee@gmail.com>
2020-11-06 11:23:22 +0900

    3주차: player와 게임 스테이트 분리

--- TermProject/game_state.py
+++ TermProject/game_state.py
@@ -2,11 +2,7 @@ import math
 from pico2d import*
 
 import core
-
-# HACK: player 클래스를 따로 마련하자
-ACC_VELOCITY = 50.0
-DEC_VELOCITY = 120.0
-MAX_VELOCITY = 500.0
+from player import Player
 
 class GameState:
     def init(self):
@@ -21,11 +17,7 @@ class GameState:
         boundary = core.sprite.Sprite('./res/boundary.png')
         core.renderer.Add(boundary)
 
-        self.player = core.sprite.Sprite('./res/player.png')
-        core.renderer.Add(self.player)
-
-        self.player_speed_x = 0.0
-        self.player_speed_y = 0.0
+        self.player = Player()
 
         self.zoom_point = core.sprite.Sprite('./res/zoom_point.png')
         self.zoom_point.camera_ignorer = True
@@ -42,66 +34,12 @@ class GameState:
             core.pop_state()
             return
 
-        move_dir_x = core.eh.get_key(SDLK_d) - core.eh.get_key(SDLK_a)
-        move_dir_y = core.eh.get_key(SDLK_w) - core.eh.get_key(SDLK_s)
+        view_dir_x = self.zoom_point.x + core.camera.x - self.player.spr.x
+        view_dir_y = self.zoom_point.y + core.camera.y - self.player.spr.y
 
-        if move_dir_x != 0.0:
-            if move_dir_x > 0.0:
-                self.player_speed_x = min(self.player_speed_x + ACC_VELOCITY, MAX_VELOCITY)
-            else:
-                self.player_speed_x = max(self.player_speed_x - ACC_VELOCITY, -MAX_VELOCITY)
-        else:
-            if self.player_speed_x > 0.0:
-                self.player_speed_x = max(self.player_speed_x - DEC_VELOCITY, 0.0)
-            else:
-                self.player_speed_x = min(self.player_speed_x + DEC_VELOCITY, 0.0)
-
-        if move_dir_y != 0.0:
-            if move_dir_y > 0.0:
-                self.player_speed_y = min(self.player_speed_y + ACC_VELOCITY, MAX_VELOCITY)
-            else:
-                self.player_speed_y = max(self.player_speed_y - ACC_VELOCITY, -MAX_VELOCITY)
-        else:
-            if self.player_speed_y > 0.0:
-                self.player_speed_y = max(self.player_speed_y - DEC_VELOCITY, 0.0)
-            else:
-                self.player_speed_y = min(self.player_speed_y + DEC_VELOCITY, 0.0)
-
-        final_speed_x = self.player_speed_x
-        final_speed_y = self.player_speed_y
-
-        if final_speed_x != 0.0 and final_speed_y != 0.0:
-            speed_size = math.sqrt(final_speed_x ** 2 + final_speed_y ** 2)
-            final_speed_x *= abs(final_speed_x) / speed_size
-            final_speed_y *= abs(final_speed_y) / speed_size
-
-        self.player.x += final_speed_x * core.delta_time
-        self.player.y += final_speed_y * core.delta_time
-
-        self.zoom_point.x = core.eh.mouse_pos[0]
-        self.zoom_point.y = core.eh.mouse_pos[1]
-        self.zoom_outer.x = core.eh.mouse_pos[0]
-        self.zoom_outer.y = core.eh.mouse_pos[1]
-
-        view_dir_x = self.zoom_point.x + core.camera.x - self.player.x
-        view_dir_y = self.zoom_point.y + core.camera.y - self.player.y
-        zoom_dis = math.sqrt(view_dir_x ** 2 + view_dir_y ** 2)
-
-        self.zoom_scale = min(max(1.0, zoom_dis / 300.0), 1.5)
-        self.zoom_outer.scaleX = self.zoom_scale
-        self.zoom_outer.scaleY = self.zoom_scale
-
-        if zoom_dis != 0.0:
-            view_dir_x /= zoom_dis
-            view_dir_y /= zoom_dis
-        
-        self.player.angle = math.degrees(math.atan2(view_dir_y, view_dir_x)) - 90.0
-
-        CAMERA_VELOCITY = 8
-        target_x = self.player.x - core.const.SCREEN_WIDTH / 2
-        target_y = self.player.y - core.const.SCREEN_HEIGHT / 2
-        core.camera.x += (target_x - core.camera.x) * CAMERA_VELOCITY * core.delta_time
-        core.camera.y += (target_y - core.camera.y) * CAMERA_VELOCITY * core.delta_time
+        self.__update_zoom(view_dir_x, view_dir_y)
+        self.player.update(view_dir_x, view_dir_y)
+        self.__update_camera()
 
     def exit(self):
         core.renderer.clear()
@@ -111,6 +49,28 @@ class GameState:
 
     def resume(self):
         pass
+
+    def __update_zoom(self, view_dir_x, view_dir_y):
+        self.zoom_point.x = core.eh.mouse_pos[0]
+        self.zoom_point.y = core.eh.mouse_pos[1]
+        self.zoom_outer.x = core.eh.mouse_pos[0]
+        self.zoom_outer.y = core.eh.mouse_pos[1]
+
+        zoom_dis = math.sqrt(view_dir_x ** 2 + view_dir_y ** 2)
+        if zoom_dis != 0.0:
+            view_dir_x /= zoom_dis
+            view_dir_y /= zoom_dis
+
+        self.zoom_scale = min(max(1.0, zoom_dis / 300.0), 1.5)
+        self.zoom_outer.scaleX = self.zoom_scale
+        self.zoom_outer.scaleY = self.zoom_scale
+
+    def __update_camera(self):
+        CAMERA_VELOCITY = 8
+        target_x = self.player.spr.x - core.const.SCREEN_WIDTH / 2
+        target_y = self.player.spr.y - core.const.SCREEN_HEIGHT / 2
+        core.camera.x += (target_x - core.camera.x) * CAMERA_VELOCITY * core.delta_time
+        core.camera.y += (target_y - core.camera.y) * CAMERA_VELOCITY * core.delta_time
 
 if __name__ == '__main__':
     core.init(GameState())
---
+++ TermProject/player.py
@@ -0,0 +1,60 @@
+from pico2d import *
+
+import core
+
+ACC_VELOCITY = 50.0
+DEC_VELOCITY = 120.0
+MAX_VELOCITY = 500.0
+
+class Player:
+    def __init__(self):
+        self.spr = core.sprite.Sprite('./res/player.png')
+        core.renderer.Add(self.spr)
+
+        self.speed_x = 0.0
+        self.speed_y = 0.0
+
+    def __del__(self):
+        pass
+
+    def update(self, view_dir_x, view_dir_y):
+        move_dir_x = core.eh.get_key(SDLK_d) - core.eh.get_key(SDLK_a)
+        move_dir_y = core.eh.get_key(SDLK_w) - core.eh.get_key(SDLK_s)
+
+        if move_dir_x != 0.0:
+            if move_dir_x > 0.0:
+                self.speed_x = min(self.speed_x + ACC_VELOCITY, MAX_VELOCITY)
+            else:
+                self.speed_x = max(self.speed_x - ACC_VELOCITY, -MAX_VELOCITY)
+        else:
+            if self.speed_x > 0.0:
+                self.speed_x = max(self.speed_x - DEC_VELOCITY, 0.0)
+            else:
+                self.speed_x = min(self.speed_x + DEC_VELOCITY, 0.0)
+
+        if move_dir_y != 0.0:
+            if move_dir_y > 0.0:
+                self.speed_y = min(self.speed_y + ACC_VELOCITY, MAX_VELOCITY)
+            else:
+                self.speed_y = max(self.speed_y - ACC_VELOCITY, -MAX_VELOCITY)
+        else:
+            if self.speed_y > 0.0:
+                self.speed_y = max(self.speed_y - DEC_VELOCITY, 0.0)
+            else:
+                self.speed_y = min(self.speed_y + DEC_VELOCITY, 0.0)
+
+        final_speed_x = self.speed_x
+        final_speed_y = self.speed_y
+
+        if final_speed_x != 0.0 and final_speed_y != 0.0:
+            speed_size = math.sqrt(final_speed_x ** 2 + final_speed_y ** 2)
+            final_speed_x *= abs(final_speed_x) / speed_size
+            final_speed_y *= abs(final_speed_y) / speed_size
+
+        self.spr.x += final_speed_x * core.delta_time
+        self.spr.y += final_speed_y * core.delta_time
+
+        self.spr.angle = math.degrees(math.atan2(view_dir_y, view_dir_x)) - 90.0
+
+    def __A(self):
+        pass
